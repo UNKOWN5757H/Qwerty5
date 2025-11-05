99a95321366a1b3f26f026c72a748383b2d2d1f2
UNKOWN5757H <65911268+UNKOWN5757H@users.noreply.github.com>
2025-11-05 22:35:38 +0530

    Create web_server.py

---
+++ plugins/web_server.py
@@ -0,0 +1,15 @@
+# This is a minimal web server to pass health checks on Koyeb/Render
+# It does NOT stream files.
+
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response({"status": "running"})
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
