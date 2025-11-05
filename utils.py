2abe324732a311c3c9768addfa32e41e7718340e
UNKOWN5757H <65911268+UNKOWN5757H@users.noreply.github.com>
2025-11-05 13:45:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,3 @@
-# Don't Remove Credit @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 import logging, asyncio, os, re, random, pytz, aiohttp, requests, string, json, http.client
 from info import *
@@ -736,4 +733,5 @@ async def get_seconds(time_string):
         return value * 86400 * 365
     else:
         return 0
+
 
